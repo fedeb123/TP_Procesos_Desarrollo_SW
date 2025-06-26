46b2a6b821bdce0d256ae6022cb977f2047b9622
fedeb123 <federicoab@gmail.com>
2025-06-26 12:35:44 -0300

    re agregado del Job para terminar el partido en juego

--- src/main/java/com/uade/tpo/jobs/PartidoJob.java
+++ src/main/java/com/uade/tpo/jobs/PartidoJob.java
@@ -6,15 +6,16 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import com.uade.tpo.models.Enums;
 import com.uade.tpo.models.Partido;
 import com.uade.tpo.repositories.PartidoRepository;
 
 
 public class PartidoJob {
-    private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(2);
 
     public void start() {
-        Runnable task = () -> {
+        Runnable iniciarPartidoConfirmado = () -> {
             ArrayList<Partido> partidos = PartidoRepository.getInstance().getPartidos();
             Date now = new Date();
             System.out.printf("Revisando hora actual: %s %n", now);
@@ -27,11 +28,28 @@ public class PartidoJob {
             }
         };
 
+        Runnable terminarPartidoEnJuego = () -> {
+            ArrayList<Partido> partidos = PartidoRepository.getInstance().getPartidos();
+            Date now = new Date();
+            for (Partido partido : partidos) {
+                if (partido.getEstado().toString() == null ? Enums.TipoEstadoPartido.EN_JUEGO.toString() == null : partido.getEstado().toString().equals(Enums.TipoEstadoPartido.EN_JUEGO.toString())) {
+                    Date fin = new Date(partido.getHorario().getTime() + TimeUnit.MINUTES.toMillis((long) partido.getDuracionEncuentro()));
+                    if (!now.before(fin)) {
+                        System.out.println("Transicionando partido a TERMINADO: " + partido.getDireccion());
+                        partido.finalizar(); // que cambie el estado a TERMINADO
+                    }
+                }
+            }
+        };
+
         // Ejecuta la tarea cada minuto
-        scheduler.scheduleAtFixedRate(task, 0, 1, TimeUnit.MINUTES);
+        scheduler.scheduleAtFixedRate(iniciarPartidoConfirmado, 0, 1, TimeUnit.MINUTES);
+        scheduler.scheduleAtFixedRate(terminarPartidoEnJuego, 0, 1, TimeUnit.MINUTES);
+
     }
 
     public void stop() {
         scheduler.shutdown();
     }
 }
+
